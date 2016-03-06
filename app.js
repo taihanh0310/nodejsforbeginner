e57bf842931d536f4c8373aed972750392f05a45
Nguyen Tai Hanh <taihanh0310@gmail.com>
2016-03-06 22:48:52 +0700

    init commit
    test project

---
+++ app.js
@@ -0,0 +1,12 @@
+ var http = require('http');
+    http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Hello World\n');
+    }).listen(1337, "127.0.0.1");
+    console.log('Server running at http://127.0.0.1:1337/');
+
+    function plus(a, b){
+    	return a+b;
+    }
+
+ console.log(plus(10,12));
