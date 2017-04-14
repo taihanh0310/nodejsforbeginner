d3654ac52544326de2c31bb628cfe2f84d602c69
Nguyen Tai Hanh <hanhnt@3forcom.com>
2017-04-14 15:32:30 +0700

    update

---
+++ CrudExpressMongoDB/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+//https://zellwk.com/blog/crud-express-mongodb/
+//https://zellwk.com/blog/crud-express-and-mongodb-2/
+// https://mlab.com/databases/star-wars-quotes/collections/quotes
+
+// set view engine
+app.set('view engine', 'ejs');
+// Once the view engine is set, we can begin generating the HTML with our quotes.
+
+var db;
+MongoClient.connect('mongodb://admin:123456@ds161400.mlab.com:61400/star-wars-quotes', (err, database) => {
+	if(err) {
+		return console.log(err);
+	}
+
+	db = database;
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.listen(3000, () => {
+		console.log('listening on 3000');
+	});
+
+	app.get('/', function(request, response){
+		var cursor = db.collection('quotes').find().toArray(function(err, results) {
+		  	if (err)
+		  	{
+		  		 return console.log(err);
+		  	}
+		    // renders index.ejs
+		    response.render('index.ejs', {quotes: results});
+		});
+		console.log(cursor);
+	});
+
+	// post
+	app.post('/quotes', (req, res) => {
+	  db.collection('quotes').save(req.body, (err, result) => {
+	    if (err) return console.log(err);
+
+	    console.log('saved to database');
+	    res.redirect('/');
+	  })
+	})
+});
+
+// 
+
+
+// app.listen(3000,function() {
+// 	console.log('listen on 3000');
+// });
+
+
+
+// /*Post*/
+// app.post('/quotes', (req, res) => {
+// 	// console.log("Hellooooooooooo");
+// 	console.log(req.body)
+// });
